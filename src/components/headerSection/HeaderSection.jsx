f4051c55023486a7b54029df2f967ccb9d482059
François Dauzet <francoisdauzet.dev@gmail.com>
2024-04-16 17:16:43 +0200

    Update theme management and add modal in about me page

--- src/components/headerSection/HeaderSection.jsx
+++ src/components/headerSection/HeaderSection.jsx
@@ -6,6 +6,9 @@ import imgProfilePath from '../../assets/images/profile.png';
 import './HeaderSection.scss';
 const HeaderSection = () => {
   useEffect(() => {
+    /*
+     * vertical parallax effect
+     */
     const handleScroll = () => {
       const scrollPos = window.scrollY;
       const containerHeaderSection = document.querySelector(
@@ -17,6 +20,24 @@ const HeaderSection = () => {
     };
 
     window.addEventListener('scroll', handleScroll);
+
+    /*
+     * modal function
+     */
+    const button = document.getElementsByClassName('contact-button')[0];
+    const modalContainer = document.getElementById('modal-container');
+
+    button.addEventListener('click', function () {
+      const buttonId = this.getAttribute('id');
+      modalContainer.removeAttribute('class');
+      modalContainer.classList.add(buttonId);
+      document.body.classList.add('modal-active');
+    });
+
+    modalContainer.addEventListener('click', function () {
+      this.classList.add('out');
+      document.body.classList.remove('modal-active');
+    });
 
     return () => {
       window.removeEventListener('scroll', handleScroll);
@@ -51,7 +72,39 @@ const HeaderSection = () => {
               Adaptable, capable de travailler aussi bien seul qu'en équipe, et
               toujours en quête d'innovation pour améliorer les projets.
             </p>
-            <a href="#">Contact me</a>
+            <div class="content">
+              <div class="buttons">
+                {/* one two five six */}
+                <div id="five" class="contact-button">
+                  Contact me
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div id="modal-container">
+        <div class="modal-background">
+          <div class="modal">
+            <h2>I'm a Modal</h2>
+            <p>Hear me roar.</p>
+            <svg
+              class="modal-svg"
+              xmlns="http://www.w3.org/2000/svg"
+              width="100%"
+              height="100%"
+              preserveAspectRatio="none"
+            >
+              <rect
+                x="0"
+                y="0"
+                fill="none"
+                width="226"
+                height="162"
+                rx="3"
+                ry="3"
+              ></rect>
+            </svg>
           </div>
         </div>
       </div>
