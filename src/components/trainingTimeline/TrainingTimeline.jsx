71c332e9f136cac391dca0964160cfd9087df39d
François Dauzet <francoisdauzet.dev@gmail.com>
2024-04-20 15:21:16 +0200

    Add link for tools in all skills section

--- src/components/trainingTimeline/TrainingTimeline.jsx
+++ src/components/trainingTimeline/TrainingTimeline.jsx
@@ -7,8 +7,12 @@ const TrainingTimeline = () => {
     <section>
       <div className="container-training-timeline">
         <h1 className="title-section">
-          <span className="first-word">#All</span>
-          <span className="second-word">Trainings</span>
+          <span translate="no" className="first-word">
+            #All
+          </span>
+          <span translate="no" className="second-word">
+            Trainings
+          </span>
         </h1>
         <p className="description">
           De la conception web et mobile à la gestion de bases de données, mes
@@ -20,13 +24,15 @@ const TrainingTimeline = () => {
           {trainingData.map((training, index) => (
             <div key={index} className="timeline-item">
               <div className="timeline-dot"></div>
-              <div className="timeline-date">{training.date}</div>
+              <div translate="no" className="timeline-date">
+                {training.date}
+              </div>
               <div className="timeline-content">
                 <img src={training.imagePath} alt="" />
                 <div className="container-text">
-                  <span>{training.certification}</span>
-                  <h3>{training.title}</h3>
-                  <h4>{training.subtitle}</h4>
+                  <span translate="no">{training.certification}</span>
+                  <h3 translate="no">{training.title}</h3>
+                  <h4 translate="no">{training.subtitle}</h4>
                   <p>{training.description}</p>
                 </div>
               </div>
