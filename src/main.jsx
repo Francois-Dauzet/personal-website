74e886e6620f60ea262026b28d69c9c9c4b9bdc3
François Dauzet <francoisdauzet.dev@gmail.com>
2024-04-24 12:06:47 +0200

    0.0.2

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import TagManager from 'react-gtm-module';
+
+const tagManagerArgs = {
+  gtmId: 'GTM-NBQ7GWGK',
+};
+
+TagManager.initialize(tagManagerArgs);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
