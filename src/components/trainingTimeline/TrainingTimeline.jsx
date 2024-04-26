92940ea1785dd8cae785bd83b01513a64e7abe69
François Dauzet <francoisdauzet.dev@gmail.com>
2024-04-26 23:40:14 +0200

    0.3.0

--- src/components/trainingTimeline/TrainingTimeline.jsx
+++ src/components/trainingTimeline/TrainingTimeline.jsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './TrainingTimeline.scss';
 import { trainingData } from '../../data/trainingData';
 import TitleSection from '../titleSection/TitleSection';
 
 const TrainingTimeline = () => {
+  const [showCertificates, setShowCertificates] = useState(false);
+  const [certificateImages, setCertificateImages] = useState([]);
+
+  useEffect(() => {
+    if (showCertificates) {
+      document.body.style.overflow = 'hidden'; // Bloque le défilement de la page arrière lorsque le carrousel est affiché
+    } else {
+      document.body.style.overflow = ''; // Rétablit le défilement de la page lorsque le carrousel est fermé
+    }
+  }, [showCertificates]);
+
+  const toggleCertificates = (images) => {
+    setCertificateImages(images);
+    setShowCertificates(!showCertificates);
+  };
+
+  const handleOverlayClick = (event) => {
+    if (event.target.classList.contains('certificate-overlay')) {
+      setShowCertificates(false);
+    }
+  };
+
+  const handleCarouselClick = (event) => {
+    event.stopPropagation(); // Empêche la propagation du clic à l'overlay
+  };
+
   return (
     <section>
       <div className="container-training-timeline">
@@ -30,11 +56,38 @@ const TrainingTimeline = () => {
                   <h3 translate="no">{training.title}</h3>
                   <h4 translate="no">{training.subtitle}</h4>
                   <p>{training.description}</p>
+                  {training.certifications.length > 0 && (
+                    <button
+                      className="certificate-button"
+                      onClick={() =>
+                        toggleCertificates(training.certifications)
+                      }
+                    >
+                      {training.certifications.length > 1
+                        ? 'View Certificates'
+                        : 'View Certificate'}
+                    </button>
+                  )}
                 </div>
               </div>
             </div>
           ))}
         </div>
+        {showCertificates && (
+          <div className={'certificate-overlay'} onClick={handleOverlayClick}>
+            <div className="certificate-carousel" onClick={handleCarouselClick}>
+              {certificateImages.map((image, index) => (
+                <img key={index} src={image} alt={`Certificate ${index + 1}`} />
+              ))}
+            </div>
+            <button
+              className="close-button"
+              onClick={() => setShowCertificates(false)}
+            >
+              Close Window
+            </button>
+          </div>
+        )}
       </div>
     </section>
   );
--- src/data/trainingData.jsx
+++ src/data/trainingData.jsx
@@ -14,6 +14,7 @@ export const trainingData = [
     description:
       'This experience has allowed for acquiring in-depth expertise in the management and optimization of IT infrastructures. DevOps practices and tools were used to automate deployments, enhance collaboration between development and operations teams, and ensure system availability, reliability, and security.',
     imagePath: imageDatascientestPath,
+    certifications: [imageCarrosseriePath, imageDatascientestPath],
   },
   {
     date: '2022 - 2023',
@@ -23,6 +24,12 @@ export const trainingData = [
     description:
       'This immersion has led to the development of in-depth expertise in the design and development of software applications. Additionally, it provided an opportunity to cultivate skills in requirements analysis, software architecture design, and project management.',
     imagePath: imageEcoleNumeriqueArdechoisePath,
+    certifications: [
+      'https://www.google.com/search?sca_esv=0e9d35a69ed69d23&sca_upv=1&rlz=1C1VDKB_frFR1094FR1094&q=fake+certificates&uds=AMwkrPufgvfzFeIw5yilA4Gzc5SOzRcpS74z-JEHBnTHK6n-h1FD9Z8RCZwYV5LzXSfjh-SMPit-YvJoIktcGJxSaWmbg_1x-T3bZkX0wBrXsCyJo4zqjvsB3nWvadkqwsqUIx6IhXwy5R4frapt-9cjGUiVeKscwjjnlwudjzdblejZe86Wtec4JEJ0k-AgwN3hm0RKtw771bwtHrxgkTkrRLnLr-CIuNL2vO56hasiWOzGbTVGimBeYVoBhPN2vqVFFOt-UlaPVd7BnASv74v6Hw9t9NJu1Kl4RfnyRfc7H_sGLB9-w7I&udm=2&prmd=invbz&sa=X&ved=2ahUKEwiw6PGE5uCFAxW9cKQEHRwnBCkQtKgLegQIDRAB&biw=1536&bih=730&dpr=1.25#vhid=IcCoydyTwzOXqM&vssid=mosaic',
+      'https://images.bannerbear.com/direct/4mGpW3zwrxA1K0AxQw/requests/000/010/384/635/MRj52Zwoa6xWMvB2zxWkdO3eE/85ef4939955b3b1903ddd7cfad60628c61ae00e4.jpg',
+      'https://www.diplomaoutlet.com/wp-content/uploads/Network-Certificate.jpg',
+      'https://diploma-makers.com/wp-content/uploads/2019/10/India-fake-diploma-sample-University-of-Kerala.jpg',
+    ],
   },
   {
     date: '2021 - 2022',
@@ -32,6 +39,7 @@ export const trainingData = [
     description:
       'This experience has conferred a comprehensive mastery in the development of web and mobile applications. I deepened my skills in programming languages such as HTML, CSS, JavaScript, as well as in the use of popular frameworks and libraries. Moreover, I gained the expertise needed to design intuitive and responsive user interfaces.',
     imagePath: imageSimplonPath,
+    certifications: [],
   },
   {
     date: '2019 - 2021',
@@ -41,6 +49,7 @@ export const trainingData = [
     description:
       'As a self-taught individual, I learned to design and develop dynamic websites using HTML, CSS, and JavaScript. Furthermore, I acquired skills in database manipulation and management by learning the SQL language.',
     imagePath: imageFormationsPath,
+    certifications: [],
   },
   {
     date: '2012 - 2013',
@@ -50,6 +59,7 @@ export const trainingData = [
     description:
       'This provided the opportunity to acquire and develop practical and theoretical skills in hive management and honey production, learning techniques such as beekeeping, honey harvesting, and disease management, all while adhering to principles of sustainable and ecological beekeeping.',
     imagePath: imageApicultureGroupPath,
+    certifications: [imageDatascientestPath, imageDatascientestPath],
   },
   {
     date: '2007 - 2008',
@@ -59,5 +69,6 @@ export const trainingData = [
     description:
       'This immersion allowed for the development of skills necessary to perform repairs and restoration work on the structure and exterior appearance of vehicles. This includes repairing deformations, painting, and replacing damaged elements, all while adhering to safety and quality standards.',
     imagePath: imageCarrosseriePath,
+    certifications: [imageDatascientestPath, imageDatascientestPath],
   },
 ];
